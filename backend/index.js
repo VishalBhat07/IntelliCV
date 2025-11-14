161a34db463d06b9337569e9a47cc317beaa9916
VishalBhat07 <vishalbhat21092005@gmail.com>
2025-11-14 20:40:31 +0530

    feat: configure mysql database using sequelize

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,25 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const { sequelize } = require("./config/db");
+// const authRoutes = require("./src/routes/authRoutes");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// base route
 app.get("/", (req, res) => {
-    res.send("IntelliCV Backend Running");
+  res.send("IntelliCV Backend is running...");
 });
 
-module.exports = app;
+// API routes
+// app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT;
+
+sequelize.sync().then(() => {
+  console.log("MySQL Database Connected");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
